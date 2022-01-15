e1ae5aca168f5926c264ae7f0a0f3d89b0f52398
Reagan Ekhameye <reaganekhameye@gmail.com>
2022-01-15 16:04:08 +0100

    feat: add job mode

---
+++ config/config.js
@@ -0,0 +1,28 @@
+// const fs = require('fs')
+const dotenv = require('dotenv')
+dotenv.config({ path: './config/.env' })
+
+module.exports = {
+	development: {
+		username: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		dialect: 'mysql',
+		dialectOptions: {
+			bigNumberStrings: true,
+		},
+	},
+	production: {
+		username: process.env.PROD_DB_USER,
+		password: process.env.PROD_DB_PASS,
+		database: process.env.PROD_DB_NAME,
+		host: process.env.PROD_DB_HOST,
+		port: process.env.PROD_DB_PORT,
+		dialect: 'mysql',
+		dialectOptions: {
+			bigNumberStrings: true,
+		},
+	},
+}
---
+++ migrations/20220115142729-create-job.js
@@ -0,0 +1,36 @@
+'use strict'
+module.exports = {
+	async up(queryInterface, DataTypes) {
+		await queryInterface.createTable('jobs', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: DataTypes.INTEGER,
+			},
+			title: {
+				type: DataTypes.STRING,
+			},
+			description: {
+				type: DataTypes.STRING,
+			},
+			type: {
+				type: DataTypes.STRING,
+			},
+			country: {
+				type: DataTypes.STRING,
+			},
+			createdAt: {
+				allowNull: false,
+				type: DataTypes.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: DataTypes.DATE,
+			},
+		})
+	},
+	async down(queryInterface, DataTypes) {
+		await queryInterface.dropTable('jobs')
+	},
+}
