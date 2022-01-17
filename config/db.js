39811b052317e7c898b856ebb9ad163cd58d77a8
Reagan Ekhameye <reaganekhameye@gmail.com>
2022-01-17 18:19:22 +0100

    fix: bug when database connection goes down

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 const { sequelize } = require('../models')
 
 const DBconnection = async () => {
-	const conn = await sequelize.authenticate().catch((err) => {
-		console.log(`For some reasons we couldn't connect to the DB`.red, err)
-	})
-
-	console.log(`MYSQL Connected: ${sequelize.config.host}`.cyan.underline.bold)
+	sequelize
+		.authenticate()
+		.then(() => {
+			console.log(
+				`MYSQL Connected: ${sequelize.config.host}`.cyan.underline.bold
+			)
+		})
+		.catch((err) => {
+			console.log(`For some reasons we couldn't connect to the DB`.red, err)
+			return
+		})
 }
 
 module.exports = DBconnection
